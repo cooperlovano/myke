f5bf1749d41a1fddcd9a4697622f1ca9082a5cdb
toni-minge <toni.minge@gmail.com>
2024-08-01 14:46:59 +0200

    chore: Update edit-symbol page layout for better responsiveness

--- src/pages/edit-symbol.jsx
+++ src/pages/edit-symbol.jsx
@@ -5,5 +5,9 @@ import "../builder-registry";
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY);
 
 export default function Page() {
-  return <BuilderComponent model="symbol" />
+  return (
+    <div className='min-h-[90vh] max-w-screen-xl p-4 mx-auto'>
+        <BuilderComponent model="symbol" />
+    </div>
+    )
 }
